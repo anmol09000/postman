e56c9633fa1c1079386888a0c40e0a137607ec16
Anmol Sharma <95350969+anmol09000@users.noreply.github.com>
2023-11-24 13:45:49 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,4 +1,5 @@
 var express = require("express");
+let axios = require("axios");
 var app = express();
 app.use(express.json());
 app.use(function (req, res, next) {
@@ -13,194 +14,33 @@ app.use(function (req, res, next) {
   );
   next();
 });
-var port = process.env.PORT||2410;
+const port = 2410;
 app.listen(port, () => console.log(`Node app listening on port ${port}!`));
 
-let baseURL = "https://jsonplaceholder.typicode.com";
-let baseURL2 = "https://repo-8qu2.onrender.com/studentServer";
-let axios = require("axios");
 
-app.get("/posts",function(req,res){
-    axios.get(baseURL+"/posts").then((response)=>{
-        res.send(response.data);
-    })
-    .catch((error)=>{
-        if(error.response){
-            let {status,statusText} = error.response;
-            res.status(status).send(statusText);
-        }else res.status(404).send(error);
-    })
-    
-})
-app.get("/posts/:id",function(req,res){
-    let {id} = req.params;
-    axios.get(`${baseURL}/posts/${id}`).then((response)=>{
-        res.send(response.data);
-    })
-    .catch((error)=>{
-        if(error.response){
-            let {status,statusText} = error.response;
-            res.status(status).send(statusText);
-        }else res.status(404).send(error);
-    })
-    
-})
-app.get("/posts/:postId/comments",function(req,res){
-    let {postId} = req.params;
-    axios.get(`${baseURL}/posts/${postId}/comments`).then((response)=>{
-        res.send(response.data);
-    })
-    .catch((error)=>{
-        if(error.response){
-            let {status,statusText} = error.response;
-            res.status(status).send(statusText);
-        }else res.status(404).send(error);
-    })
-    
-})
-app.get("/comments",function(req,res){
-    let {postId} = req.query;
-    let params={};
-    if(postId) params.postId = postId;
-    axios.get(baseURL+"/comments",{params:params}).then((response)=>{
-        res.send(response.data);
-    })
-    .catch((error)=>{
-        if(error.response){
-            let {status,statusText} = error.response;
-            res.status(status).send(statusText);
-        }else res.status(404).send(error);
-    })
-    
-})
-app.post("/posts",function(req,res){
-    let body = req.body;
-    axios.post(baseURL + "/posts" , body).then((response)=>{
-        res.send(response.data);
-    })
-    .catch((error)=>{
-        if(error.response){
-            let {status,statusText} = error.response;
-            res.status(status).send(statusText);
-        }else res.status(404).send(error);
-    })
-    
-})
-app.put("/posts/:id",function(req,res){
-    let {id} = req.params;
-    let body = req.body;
-    axios.put(`${baseURL}/posts/${id}`, body).then((response)=>{
-        res.send(response.data);
-    })
-    .catch((error)=>{
-        if(error.response){
-            let {status,statusText} = error.response;
-            res.status(status).send(statusText);
-        }else res.status(404).send(error);
-    })
-    
-})
-app.delete("/posts/:id",function(req,res){
-    let {id} = req.params;
-    axios.delete(`${baseURL}/posts/${id}`).then((response)=>{
-        res.send(response.data);
-    })
-    .catch((error)=>{
-        if(error.response){
-            let {status,statusText} = error.response;
-            res.status(status).send(statusText);
-        }else res.status(404).send(error);
-    })
-    
-})
+app.post('/callEndpoint', async (req, res) => {
+  try{
+    let {method, fetchURL, data} = req.body;
+    let token = req.headers["authorization"];
+    if(method === "GET"){
+      let response = await axios.get(fetchURL,{headers:{Authorization:token}});
+      res.json(response.data);
+    }else if(method === "POST"){
+      let response = await axios.post(fetchURL,data,{headers:{Authorization:token}});
+      res.json(response.data)
+    }else if(method === "PUT"){
+      let response = await axios.put(fetchURL,data,{headers:{Authorization:token}});
+      res.json(response.data)
+    }else if(method === "DELETE"){
+      let response = await axios.delete(fetchURL,{headers:{Authorization:token}});
+      res.json(response.data)
+    }
+  }
+  catch(error){
+    if(error.response){
+      let {status,statusText} = error.response;
+      res.status(status).send(statusText);
+    }
+  }
+});
 
-app.get("/getToken",function(req,res){
-    axios.get(baseURL2+"/getToken").then(function(response){
-      res.send(""+response.data);
-    })
-    .catch(function(error){
-      if(error.response){
-        let {status,statusText} = error.response;
-        res.status(status).send(statusText);
-      }else res.status(404).send(error);
-    })
-  })
-  app.get("/students",function(req,res){
-    let token = req.headers["authorization"];
-    axios.get(baseURL2+"/students",{headers:{authorization:token}}).then(function(response){
-      res.send(response.data);
-    })
-    .catch(function(error){
-      if(error.response){
-        let {status,statusText} = error.response;
-        res.status(status).send(statusText);
-      }else res.status(404).send(error);
-    })
-  })
-  app.get("/students/:id",function(req,res){
-    let {id} = req.params;
-    let token = req.headers["authorization"];
-    axios.get(`${baseURL2}/students/${id}`,{headers:{authorization:token}}).then(function(response){
-      res.send(response.data);
-    })
-    .catch(function(error){
-      if(error.response){
-        let {status,statusText} = error.response;
-        res.status(status).send(statusText);
-      }else res.status(404).send(error);
-    })
-  })
-  app.get("/students/course/:name",function(req,res){
-    let {name} = req.params;
-    let token = req.headers["authorization"];
-    axios.get(`${baseURL2}/students/course/${name}`,{headers:{authorization:token}}).then(function(response){
-      res.send(response.data);
-    })
-    .catch(function(error){
-      if(error.response){
-        let {status,statusText} = error.response;
-        res.status(status).send(statusText);
-      }else res.status(404).send(error);
-    })
-  })
-  app.post("/students",function(req,res){
-    let body = req.body;
-    let token = req.headers["authorization"];
-    axios.post(baseURL2 + "/students",body,{headers:{authorization:token}}).then(function(response){
-      res.send(response.data);
-    })
-    .catch(function(error){
-      if(error.response){
-        let {status,statusText} = error.response;
-        res.status(status).send(statusText);
-      }else res.status(404).send(error);
-    })
-  })
-  app.put("/students/:id",function(req,res){
-    let {id} = req.params;
-    let body = req.body;
-    let token = req.headers["authorization"];
-    axios.put(`${baseURL2}/students/${id}`,body,{headers:{authorization:token}}).then(function(response){
-      res.send(response.data);
-    })
-    .catch(function(error){
-      if(error.response){
-        let {status,statusText} = error.response;
-        res.status(status).send(statusText);
-      }else res.status(404).send(error);
-    })
-  })
-  app.delete("/students/:id",function(req,res){
-    let {id} = req.params;
-    let token = req.headers["authorization"];
-    axios.delete(`${baseURL2}/students/${id}`,{headers:{authorization:token}}).then(function(response){
-      res.send(response.data);
-    })
-    .catch(function(error){
-      if(error.response){
-        let {status,statusText} = error.response;
-        res.status(status).send(statusText);
-      }else res.status(404).send(error);
-    })
-  });
-  
